4c6dfbd08ad9567dcdc0bc035eda8f930f3e91fa
Shaikh Faizan <skfaiz0929@gmail.com>
2023-04-02 21:04:20 +0530

    saved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  ApexAxisChartSeries,
+  ApexTitleSubtitle,
+  ApexDataLabels,
+  ApexChart,
+  ApexPlotOptions,
+  ApexLegend,
+  ChartComponent,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries | any;
+  chart: ApexChart | any;
+  dataLabels: ApexDataLabels | any;
+  title: ApexTitleSubtitle | any;
+  plotOptions: ApexPlotOptions | any;
+  legend: ApexLegend | any;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'apex';
+  @ViewChild('chart') chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          data: [
+            {
+              x: 'New Delhi',
+              y: 218,
+            },
+            {
+              x: 'Kolkata',
+              y: 149,
+            },
+            {
+              x: 'Mumbai',
+              y: 184,
+            },
+            {
+              x: 'Ahmedabad',
+              y: 55,
+            },
+            {
+              x: 'Bangaluru',
+              y: 84,
+            },
+            {
+              x: 'Pune',
+              y: 31,
+            },
+            {
+              x: 'Chennai',
+              y: 70,
+            },
+            {
+              x: 'Jaipur',
+              y: 30,
+            },
+            {
+              x: 'Surat',
+              y: 44,
+            },
+            {
+              x: 'Hyderabad',
+              y: 68,
+            },
+            {
+              x: 'Lucknow',
+              y: 28,
+            },
+            {
+              x: 'Indore',
+              y: 19,
+            },
+            {
+              x: 'Kanpur',
+              y: 29,
+            },
+          ],
+        },
+      ],
+
+      chart: {
+        height: 350,
+        type: 'treemap',
+      },
+      title: {
+        text: 'Basic Treemap',
+      },
+    };
+  }
+
+  public generateData(count: number, yrange: any) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = 'w' + (i + 1).toString();
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push({
+        x: x,
+        y: y,
+      });
+      i++;
+    }
+    return series;
+  }
 }
